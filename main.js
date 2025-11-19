e55617b986426ca441cef689859feedde046db50
mmurony33 <mmuronydaraz@gmail.com>
2025-11-19 16:44:11 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["System Administrator","Web Developer", "Youtube Gamer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
